e507949e8a481eb40c19e4c82aa83e901b0a1009
Tu Nombre <you@example.com>
2022-07-29 12:37:24 +0200

    funsiona sin herencia

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+interface FormFields{
+  product:string,
+  price:number,
+  stocks:number
+
+}
+
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css']
 })
+
+
 export class BasicosComponent implements OnInit {
 
 
   @ViewChild('myForm') myForm!:NgForm;
+
+  initForm:FormFields={
+    product:'',
+    price:0,
+    stocks:0
+  }
   constructor() { }
 
   ngOnInit(): void {
@@ -17,7 +32,8 @@ export class BasicosComponent implements OnInit {
 
   save(){
     console.log('Posteo correcto');
-    this.myForm.resetForm();
+    this.myForm.resetForm(this.initForm
+      );
   }
 
   formFieldIsValid(field:string):boolean{
--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BasicosComponent } from '../basicos/basicos.component';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styleUrls: ['./dinamicos.component.css']
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent  implements OnInit {
 
-  constructor() { }
-
+  @ViewChild('myForm') myForm!:NgForm;
   ngOnInit(): void {
+
   }
+  constructor(){
+
+  };
+
+  save(): void {
+    console.log(this.myForm)
+  }
+
+  formFieldIsValid(field:string):boolean{
+
+    return this.myForm?.form.controls[field]?.status==='INVALID' && this.myForm?.form.controls[field]?.touched
+
+  }
+
+
+
 
 }

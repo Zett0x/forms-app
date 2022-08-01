a0756ba3e186c56ce56885bd23f85231c94852e2
Tu Nombre <you@example.com>
2022-08-01 15:09:07 +0200

    section 16 finished

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -2,12 +2,40 @@ import { Component, OnInit,ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { BasicosComponent } from '../basicos/basicos.component';
 
+
+interface Persona{
+  nombre:string;
+  favoritos:Favorito[];
+}
+interface Favorito{
+  id:number;
+  nombre:string
+
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styleUrls: ['./dinamicos.component.css']
 })
 export class DinamicosComponent extends BasicosComponent implements OnInit {
+
+
+
+  nuevoJuego:string='';
+  persona:Persona={
+    nombre:'fran',
+    favoritos:[
+      {
+        id:1,
+        nombre:'Metal Gear'
+      },
+      {
+        id:2,
+        nombre:'Dead Stranding'
+      }
+    ]
+  }
 
 
   @ViewChild('myForm') override myForm!:NgForm;
@@ -18,6 +46,21 @@ export class DinamicosComponent extends BasicosComponent implements OnInit {
   constructor(){
     super();
   };
+
+  addGame(){
+    const nuevoFavorito:Favorito={
+      id:this.persona.favoritos.length+1,
+      nombre:this.nuevoJuego
+    }
+    this.nuevoJuego='';
+    this.persona.favoritos.push({...nuevoFavorito});
+
+  }
+
+
+  deleteFavourite(index:number){
+    this.persona.favoritos.splice(index,1);
+  }
 
 
 
--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styleUrls: ['./switches.component.css']
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent  {
+
+  persona={
+    genero:'',
+    notificaciones:false,
+  }
+  termsAndConditions:boolean=false;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+
 
 }
